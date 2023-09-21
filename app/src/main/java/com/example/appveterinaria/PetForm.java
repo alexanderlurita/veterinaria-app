b4e2b4bf0723d03b7f19cb4c454cf6cd8dfd63ff
Alexander Lurita <alex11lurita@gmail.com>
2023-09-21 11:26:38 -0500

    Traer datos del cliente para el registro de mascota

--- app/src/main/java/com/example/appveterinaria/PetForm.java
+++ app/src/main/java/com/example/appveterinaria/PetForm.java
@@ -1,9 +1,12 @@
 package com.example.appveterinaria;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -11,13 +14,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -25,16 +35,27 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
 
 public class PetForm extends AppCompatActivity {
 
-    Button btOpenGallery;
+    EditText etDNIBuscar, etNombresDuenio, etNombreMascota, etColor;
     Spinner spListaRazas;
+    RadioButton rbMacho, rbHembra;
+    ImageView ivFotografia;
+    Button btBuscarCliente, btSeleccionarFoto, btRegistrarMascota;
+
+    String idCliente, idRaza, nombreMascota, fotografia, color, genero;
     List<String> listaRazas = new ArrayList<>();
     private List<String> idsRazas = new ArrayList<>();
-    private static final int PICK_IMAGE_REQUEST = 1;
 
+    Bitmap bitmap;
     Context context = this;
 
     @Override
@@ -43,29 +64,88 @@ public class PetForm extends AppCompatActivity {
         setContentView(R.layout.activity_pet_form);
 
         loadUI();
-
         getRaces();
 
-        spListaRazas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = parent.getItemAtPosition(position).toString();
-                String selectedId = idsRazas.get(position);
-                Toast.makeText(getApplicationContext(), "Seleccionaste: " + selectedItem + " (ID: " + selectedId + ")", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        btOpenGallery.setOnClickListener(new View.OnClickListener() {
+        btBuscarCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+                searchClient();
             }
         });
+
+        btSeleccionarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+    }
+
+    public String getStringImagen(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void registerPet() {
+        String URL = Utils.URL + "mascota.controller.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    Utils.showToast(context, "Problemas al registrar");
+                } else {
+                    Utils.showToast(context, "Registrado correctamente");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Utils.showToast(context, "Problemas con el servidor");
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("operacion", "add");
+                parametros.put("idcliente", idCliente);
+                parametros.put("idraza", idRaza);
+                parametros.put("nombre", nombreMascota);
+                fotografia = getStringImagen(bitmap);
+                parametros.put("fotografia", fotografia);
+                parametros.put("color", color);
+                parametros.put("genero", genero);
+                return parametros;
+            }
+        };
+
+        Volley.newRequestQueue(context).add(stringRequest);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Selecciona imagen"), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                System.out.println(bitmap);
+                ivFotografia.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void getRaces() {
@@ -81,7 +161,6 @@ public class PetForm extends AppCompatActivity {
                         try {
                             for (int i = 0; i < response.length(); i++) {
                                 JSONObject jsonObject = response.getJSONObject(i);
-                                System.out.println(jsonObject);
                                 String idRaza = jsonObject.getString("idraza");
                                 String animalRaza = jsonObject.getString("animalraza");
 
@@ -107,9 +186,56 @@ public class PetForm extends AppCompatActivity {
         Volley.newRequestQueue(context).add(jsonArrayRequest);
     }
 
+    private void searchClient() {
+        String dni = etDNIBuscar.getText().toString().trim();
+        String URL = Utils.URL + "cliente.controller.php?operacion=search&dni=" + dni;
+
+        if (dni.isEmpty() || dni.length() < 8) {
+            Utils.showToast(context, "Escriba el DNI");
+        } else {
+            StringRequest jsonObjectRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (!response.equals("false")) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            idCliente = jsonObject.getString("idcliente");
+                            String nombresCompletos = jsonObject.getString("nombres") + " " + jsonObject.getString("apellidos");
+                            etNombresDuenio.setText(nombresCompletos);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Utils.showToast(context, "Cliente no encontrado");
+                        etNombresDuenio.setText(null);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Utils.showToast(context, "Problemas con el servidor");
+                }
+            });
+
+            Volley.newRequestQueue(context).add(jsonObjectRequest);
+        }
+    }
+
     private void loadUI() {
-        btOpenGallery = findViewById(R.id.openGalleryButton);
+        etDNIBuscar = findViewById(R.id.etDNIBuscar);
+        etNombresDuenio = findViewById(R.id.etNombresDuenio);
+        etNombreMascota = findViewById(R.id.etNombreMascota);
+        etColor = findViewById(R.id.etColor);
 
         spListaRazas = findViewById(R.id.spListaRazas);
+
+        rbMacho = findViewById(R.id.rbMacho);
+        rbHembra = findViewById(R.id.rbHembra);
+
+        ivFotografia = findViewById(R.id.ivFotografia);
+
+        btBuscarCliente = findViewById(R.id.btBuscarCliente);
+        btSeleccionarFoto = findViewById(R.id.btSeleccionarFoto);
+        btRegistrarMascota = findViewById(R.id.btRegistrarMascota);
     }
 }
